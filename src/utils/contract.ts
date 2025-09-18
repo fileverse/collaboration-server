bf1d78ae2e919e1d551eb8e07719d517029361b3
Nadeem <nadeem@fileverse.io>
2025-09-18 11:36:46 +0530

    add support for v2 portal

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -12,6 +12,69 @@ export const publicClient = createPublicClient({
   transport: http(config.rpcURL),
 });
 
+export const getLegacyPortalCollaboratorKeys = async (
+  contractAddress: Hex,
+  collaboratorAddress: Hex
+) => {
+  try {
+    const [_, did] = (await publicClient.readContract({
+      address: contractAddress,
+      abi: PORTAL_CONTRACT_ABI,
+      functionName: "collaboratorKeys",
+      args: [collaboratorAddress],
+    })) as [string, string];
+
+    return did;
+  } catch (error) {
+    console.error("Could not get legacy portal keys");
+    return null;
+  }
+};
+
+export const getV2PortalOwnerDid = async (contractAddress: Hex, collaboratorAddress: Hex) => {
+  try {
+    const result = (await publicClient.readContract({
+      address: contractAddress,
+      abi: [
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "",
+              type: "address",
+            },
+          ],
+          name: "collaboratorKeys",
+          outputs: [
+            {
+              internalType: "string",
+              name: "",
+              type: "string",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+      ],
+      functionName: "collaboratorKeys",
+      args: [collaboratorAddress],
+    })) as string;
+
+    return result;
+  } catch (error) {
+    console.error("Error getting v2 portal owner did:", error);
+    return null;
+  }
+};
+
+export const getCollaboratorDid = async (contractAddress: Hex, collaboratorAddress: Hex) => {
+  let did = null;
+  did = await getLegacyPortalCollaboratorKeys(contractAddress, collaboratorAddress);
+  if (did) return did;
+
+  return await getV2PortalOwnerDid(contractAddress, collaboratorAddress);
+};
+
 export const getOwnerDid = async (contractAddress: Hex, collaboratorAddress: Hex) => {
   const cacheKey = `${contractAddress}-${collaboratorAddress}`;
   const cachedResult = cache.get(cacheKey);
@@ -19,14 +82,7 @@ export const getOwnerDid = async (contractAddress: Hex, collaboratorAddress: Hex
     return cachedResult as string;
   }
 
-  const result = (await publicClient.readContract({
-    address: contractAddress,
-    abi: PORTAL_CONTRACT_ABI,
-    functionName: "collaboratorKeys",
-    args: [collaboratorAddress],
-  })) as [string, string];
-
-  const [_, did] = result;
+  const did = await getCollaboratorDid(contractAddress, collaboratorAddress);
   cache.set(cacheKey, did);
 
   return did;
